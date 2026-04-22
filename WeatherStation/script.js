5513f4c435fed370a31c4451ae0e3164f5a0a84a
Sheikh Alif <alif.sheikh@cz.lan>
2026-04-22 13:51:05 +0200

    added stuff

--- WeatherStation/script.js
+++ WeatherStation/script.js
@@ -2,9 +2,22 @@ function resetMarquee() {
     const title = document.getElementById('title');
     const box = title.getBoundingClientRect();
     title.style.animation = 'none';
-    title.offsetHeight; // force reflow
+    title.offsetHeight; 
     title.style.animation = 'move 14s steps(67) infinite';
     title.style.left = `${box.x}px`;
 }
 
 window.addEventListener('resize', resetMarquee);
+
+window.addEventListener('load', () => {
+    const showcase = document.querySelector('#data');
+    const ipField = document.querySelector('#ip');
+    ipField.addEventListener('keydown', async (e) => {
+        if (e.key === 'Enter') {
+            let response = await fetch(ipField.value);
+            response = await response.json();
+            showcase.innerHTML = JSON.stringify(response);
+        }
+    });
+});
+
