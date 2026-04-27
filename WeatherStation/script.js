43e1adb77e0372241608ef5f965783e1ede24899
Sheikh Alif <alif.sheikh@cz.lan>
2026-04-27 09:35:04 +0200

    added data in data.json, added labs data structure

--- WeatherStation/script.js
+++ WeatherStation/script.js
@@ -10,14 +10,37 @@ function resetMarquee() {
 window.addEventListener('resize', resetMarquee);
 
 window.addEventListener('load', () => {
-    const showcase = document.querySelector('#data');
+    const showLabs = document.querySelector('#labs');
+    const brightness = document.querySelector('#brightness');
+    const temperature = document.querySelector('#temperature');
+    const humidity = document.querySelector('#humidity');
+
     const ipField = document.querySelector('#ip');
     ipField.addEventListener('keydown', async (e) => {
-        if (e.key === 'Enter') {
-            let response = await fetch(ipField.value);
-            response = await response.json();
-            showcase.innerHTML = JSON.stringify(response);
-        }
+        if (e.key !== 'Enter') {
+            return;
+        }    
+
+        const response = await fetch(ipField.value);
+        const data = await response.json();
+        
+        let labs = getLabs(data);
+        console.log(labs);
     });
+
+    function getLabs(data) {
+        const labs = {};
+        for (sample of data) {
+            const [labName, workStation] = sample.position.split('-');
+            if (!(labName in labs)) {
+                labs[labName] = {};
+            }
+            if (!(workStation in labs[labName])) {
+                labs[labName][workStation] = [];
+            }
+            labs[labName][workStation].push(sample);
+        }
+        return labs;
+    }
 });
 
