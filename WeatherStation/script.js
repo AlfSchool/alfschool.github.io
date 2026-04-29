d3896fd7d1ca9424490c6bf4a4fe721d0907ef6f
Sheikh Alif <alif.sheikh@cz.lan>
2026-04-29 13:46:24 +0200

    added labs and pcs buttons

--- WeatherStation/script.js
+++ WeatherStation/script.js
@@ -11,9 +11,7 @@ window.addEventListener('resize', resetMarquee);
 
 window.addEventListener('load', () => {
     const showLabs = document.querySelector('#labs');
-    const brightness = document.querySelector('#brightness');
-    const temperature = document.querySelector('#temperature');
-    const humidity = document.querySelector('#humidity');
+    const showWorkStations = document.querySelector('#workStations');
 
     const ipField = document.querySelector('#ip');
     ipField.addEventListener('keydown', async (e) => {
@@ -24,8 +22,42 @@ window.addEventListener('load', () => {
         const response = await fetch(ipField.value);
         const data = await response.json();
         
-        let labs = getLabs(data);
-        console.log(labs);
+        const labs = getLabs(data);
+        
+        const labsNames = Object.keys(labs);
+        labsNames.forEach(labName => {
+            showLabs.innerHTML += `<div><button>${labName}</button></div>`;
+        });
+
+        for (const lab of showLabs.children) {
+            lab.children[0].addEventListener('click', () => {
+                const labChoosen = labs[lab.children[0].innerHTML];
+                const stations = Object.keys(labChoosen);
+                showWorkStations.innerHTML = '';
+                stations.forEach(stationName => {
+                    showWorkStations.innerHTML += `<div><button>${stationName}</button></div>`;
+                });
+
+                for (const station of showWorkStations.children) {
+                    station.addEventListener('click', () => {
+                        const workStation = labChoosen[station.children[0].innerHTML];
+                        updateInterface(workStation);
+                    });
+                }
+            });
+        }
+
+        function updateInterface(station) {
+            console.log(station);
+            const brightnessZone = document.querySelector('#brightness');
+            const temperatureZone = document.querySelector('#temperature');
+            const humidityZone = document.querySelector('#humidity');
+
+            temperatureZone.innerHTML = '<canvas id="temperatureGraph"></canvas>';
+            const graphTemperature = document.querySelector('#temperatureGraph');
+            const ctx = graphTemperature.getContext('2d');
+            
+        }
     });
 
     function getLabs(data) {
